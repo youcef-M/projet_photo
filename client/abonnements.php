2904772255b15525b62110725b9ccb5ef0a94c67
juliada <rossignoljulie@hotmail.fr>
2015-03-14 17:17:42 +0100

    listes amis/abonnes/abonnements

---
+++ client/abonnements.php
@@ -0,0 +1,34 @@
+<?php
+	include 'include.php';
+	isAllowed();
+?>
+<?php
+	include 'partials/header.php';
+?>
+    <body>
+		<header>
+				<a href="accueil.php">Accueil</a>
+				<a href="accueil.php"><img src="images/pascagram.png" alt="" /></a>
+				<a href="profil.php">Afficher mon profil</a>
+				<a href="deconnexion.php">D&eacute;connexion</a>
+		</header>
+		<?= flash(); ?>
+		<section id="liste">
+			<br/><h1>Liste des abonnements</h1><br/>
+			<ul id="listerelation">
+				<li><a href="afficherphoto.php" title=""><img src="./images/photo100x100.png" alt=""/>@ _ Ici pseudo</a></li>
+				<li><a href="afficherphoto.php" title=""><img src="./images/photo100x100.png" alt=""/>@ _ Ici pseudo</a></li>
+				<li><a href="afficherphoto.php" title=""><img src="./images/photo100x100.png" alt=""/>@ _ Ici pseudo</a></li>
+				<li><a href="afficherphoto.php" title=""><img src="./images/photo100x100.png" alt=""/>@ _ Ici pseudo</a></li>
+				<li><a href="afficherphoto.php" title=""><img src="./images/photo100x100.png" alt=""/>@ _ Ici pseudo</a></li>
+				<li><a href="afficherphoto.php" title=""><img src="./images/photo100x100.png" alt=""/>@ _ Ici pseudo</a></li>
+				<li><a href="afficherphoto.php" title=""><img src="./images/photo100x100.png" alt=""/>@ _ Ici pseudo</a></li>
+				<li><a href="afficherphoto.php" title=""><img src="./images/photo100x100.png" alt=""/>@ _ Ici pseudo</a></li>
+				<li><a href="afficherphoto.php" title=""><img src="./images/photo100x100.png" alt=""/>@ _ Ici pseudo</a></li>
+			</ul>
+		</section>
+
+
+<?php
+	include 'partials/footer.php';
+?>
