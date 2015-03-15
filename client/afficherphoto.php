5d529e6eb0bbf8dd36521e011aa589ee1c07655c
Youcef Medjellakh <youcef.medjellakh@gmail.com>
2015-03-15 18:16:33 +0100

    ajout vote

--- client/afficherphoto.php
+++ client/afficherphoto.php
@@ -12,6 +12,8 @@
 		$nbdislikes = getNbDislikes($post);
 		$comments = getComments($post);
 		$voted = voted($user,$post);
+		
+		
 		if($voted !== 404)
 		{
 			$note = getNote($user,$post);
@@ -25,56 +27,68 @@
 	}
 
 	$infopost=$infopost->post;
-	
+	$user = getUser($infopost->user_id);
 	getHeader();
 ?>
 
 	<section id="photo">
 		<img src="http://api-rest-youcef-m.c9.io<?=$infopost->chemin?>">
-		<p><?= '<b>Titre : </b>'.$infopost->titre.'<br/><b>Description : </b>'.$infopost->description.'<br/><b>Date de publication : </b>'.$infopost->created_at;?></p>
+		<p><?= '<b>Titre : </b>'.$infopost->titre.'<br/> <b> Auteur : </b>' . $user['username'] . '<br/> <b>Description : </b>'.$infopost->description.'<br/><b>Date de publication : </b>'.$infopost->created_at;?></p>
 	</section>
 
 	<section id="notation">
 		<p>
 		
 			<?php if ($note === 0): ?>
-				<button class="btn btn-success" href="#">
-					<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true">
-						<?= ' ' . $nblikes->likes; ?>  
-					</span>
+				<button class="btn btn-success">
+					<a href="submit_vote.php?id=<?= $post; ?>&first=1">
+						<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true">
+							<?= ' ' . $nblikes->likes; ?>  
+						</span>
+					</a>
 				</button>
-				<button class="btn btn-danger" href="afficherphoto.php">
+				<button class="btn btn-danger" >
+				<a href="submit_vote.php?id=<?= $post; ?>&first=-1">
 					<span class="glyphicon glyphicon-thumbs-down glyphicon-danger" aria-hidden="true">
 						<?= ' ' . $nbdislikes->dislikes; ?>
 					</span>
+				</a>
 				</button>
 			<?php endif ?>
 
 
 			<?php if ($note === 1): ?>
-				<button class="btn btn-success active" href="#">
-					<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true">
-						<?= ' ' . $nblikes->likes; ?>  
-					</span>
+				<button class="btn btn-success">
+					<a href="submit_vote.php?id=<?= $post; ?>&delete=1">
+						<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true">
+							<?= ' ' . $nblikes->likes; ?>  
+						</span>
+					</a>
 				</button>
-				<button class="btn btn-danger" href="afficherphoto.php">
+				<button class="btn btn-danger" >
+				<a href="submit_vote.php?id=<?= $post; ?>&new=-1">
 					<span class="glyphicon glyphicon-thumbs-down glyphicon-danger" aria-hidden="true">
 						<?= ' ' . $nbdislikes->dislikes; ?>
 					</span>
+				</a>
 				</button>
 			<?php endif ?>
 
 
 			<?php if ($note === -1): ?>
-				<button class="btn btn-success active" href="#">
-					<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true">
-						<?= ' ' . $nblikes->likes; ?>  
-					</span>
+				<button class="btn btn-success">
+					<a href="submit_vote.php?id=<?= $post; ?>&new=1">
+						<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true">
+							<?= ' ' . $nblikes->likes; ?>  
+						</span>
+					</a>
 				</button>
-				<button class="btn btn-danger" href="afficherphoto.php">
+				<button class="btn btn-danger" >
+				<a href="submit_vote.php?id=<?= $post; ?>&delete=1">
 					<span class="glyphicon glyphicon-thumbs-down glyphicon-danger" aria-hidden="true">
 						<?= ' ' . $nbdislikes->dislikes; ?>
 					</span>
+				</a>
 				</button>
 			<?php endif ?>
 			
